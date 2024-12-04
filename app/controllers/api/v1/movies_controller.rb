a263a43c4a7a399a7c6d7b3c0d07108124680978
Terra Manning <167652364+TDManning@users.noreply.github.com>
2024-12-04 14:17:05 -0700

    Revert "Feature/movie search 2"

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,15 +1,7 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    if params[:query].present?
-      movies = Movie.query_movies(params[:query])
-    elsif params[:filter] == 'top_rated'
-      movies = Movie.top_rated
-    else
-      movies = []
-    end
-
+    movies = Movie.top_rated
     render json: MovieSerializer.new(movies).serializable_hash
   end
 end
-
 
