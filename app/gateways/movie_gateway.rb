e5653a0894dc313e1b8923b03d31e6f8751dd858
Terra Manning <terradmanning@gmail.com>
2024-12-05 19:02:42 -0700

    Add detailed logging to search_movies

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -10,8 +10,27 @@ class MovieGateway
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
+  # def self.search_movies(query)
+  #   response = conn.get('search/movie', { query: query, api_key: Rails.application.credentials.dig(:tmdb, :api_key) })
+  #   JSON.parse(response.body, symbolize_names: true)[:results]
+  # end
+
+  
   def self.search_movies(query)
-    response = conn.get('search/movie', { query: query, api_key: Rails.application.credentials.dig(:tmdb, :api_key) })
-    JSON.parse(response.body, symbolize_names: true)[:results]
+    api_key = Rails.application.credentials.dig(:tmdb, :api_key)
+    clean_query = query.strip # Remove newline and extra spaces
+    response = conn.get('search/movie', { query: clean_query, api_key: api_key })
+  
+    Rails.logger.info("Request URL: #{response.env.url}")
+    Rails.logger.info("Response status: #{response.status}")
+    Rails.logger.info("Response body: #{response.body}")
+  
+    data = JSON.parse(response.body, symbolize_names: true) rescue nil
+    return [] if data.nil? || !data[:results].is_a?(Array)
+  
+    data[:results]
   end
+  
+  
+  
 end
