c1af1007ebef39ed087d8977d710048ce78528ad
Terra Manning <terradmanning@gmail.com>
2024-12-05 18:33:07 -0700

    fix: debug for deployment endpoint errors

--- app/serializers/viewing_party_serializer.rb
+++ app/serializers/viewing_party_serializer.rb
@@ -1,3 +1,23 @@
 class ViewingPartySerializer
+  include JSONAPI::Serializer
+  attributes :name, :start_time, :end_time, :movie_id, :movie_title, :host_id
 
+  def self.format_viewing_party_list(viewing_parties)
+    { data:
+        viewing_parties.map do |viewing_party|
+          {
+            id: viewing_party.id.to_s,
+            type: "viewing_party",
+            attributes: {
+              name: viewing_party.name,
+              start_time: viewing_party.start_time,
+              end_time: viewing_party.end_time,
+              movie_id: viewing_party.movie_id,
+              movie_title: viewing_party.movie_title,
+              host_id: viewing_party.host_id
+            }
+          }
+        end
+    }
+  end
 end
---
+++ spec/requests/api/v1/viewing_parties_request_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Viewing Parties API", type: :request do
+  describe "Create Viewing Parties Endpoint" do
+
+  end
+end
