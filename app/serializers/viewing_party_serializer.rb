9ff679646071c9d30c99bef64b40b6147a05de79
Terra Manning <terradmanning@gmail.com>
2024-12-06 09:37:44 -0800

    feat: add tests for viewing party and sad path/edge cases and custom attributes

--- app/serializers/viewing_party_serializer.rb
+++ app/serializers/viewing_party_serializer.rb
@@ -11,29 +11,4 @@ class ViewingPartySerializer
       }
     end
   end
-
-  def self.format_viewing_party_list(viewing_parties)
-    { data:
-        viewing_parties.map do |viewing_party|
-          {
-            id: viewing_party.id.to_s,
-            type: "viewing_party",
-            attributes: {
-              name: viewing_party.name,
-              start_time: viewing_party.start_time,
-              end_time: viewing_party.end_time,
-              movie_id: viewing_party.movie_id,
-              movie_title: viewing_party.movie_title,
-              invitees: viewing_party.invitees.map do |invitee|
-                {
-                  id: invitee.id,
-                  name: invitee.name,
-                  username: invitee.username
-                }
-              end
-            }
-          }
-        end
-    }
-  end
 end
